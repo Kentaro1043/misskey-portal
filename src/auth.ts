fa47493b8a472ad0dd0a4070937b39699c038e10
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-03-14 02:36:37 +0900

    🎉 Auth.jsの初期化

---
+++ src/auth.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [{
+	id: "misskey",
+	name: "Misskey",
+	type: "oauth",
+	issuer: process.env.MISSKEY_URL,
+	
+  }],
+})
