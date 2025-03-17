a5063ce8fcaa5aaf9ce25205652c88848ef2109e
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-03-17 16:02:05 +0900

    🚧 WIP

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,10 @@
-import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function OAuth() {
+export default function About() {
 	return (
 		<>
-			<Head>
-				<link rel="redirect_uri" href="/api/auth/callback/misskey" />
-			</Head>
+			<link rel="redirect_uri" href="/api/auth/callback/misskey" />
 			<div className="h-app">
 				<Image src="/logo.png" className='u-logo' alt='application logo' width={114} height={114}/>
 				<Link href='/' className='u-url p-name'>Misskey Portal</Link>
--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,26 @@
 import NextAuth from "next-auth"
- 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [{
-	id: "misskey",
-	name: "Misskey",
-	type: "oauth",
-	issuer: process.env.MISSKEY_URL,
-  }],
+  providers: [
+    {
+      id: "misskey",
+      name: "Misskey",
+      type: "oauth",
+	    issuer: process.env.MISSKEY_URL,
+      authorization: {
+		    url: process.env.MISSKEY_URL + "/oauth/authorize",
+		    params: {
+			    scope: "read:account",
+		    }
+	    },
+      token: {
+        url: process.env.MISSKEY_URL + "/oauth/token",
+        params: {
+          grant_type: "authorization_code",
+        }
+      },
+      clientId: process.env.SERVER_URL + "/about",
+      checks: [ 'state', 'pkce' ],
+    }
+  ]
 })
